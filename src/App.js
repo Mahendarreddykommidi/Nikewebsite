dc1fc2e95fc94688fd43f5684714539b7d34ab07
Mahendarreddy <mahendarredyykommidi82@gmail.com>
2024-03-30 12:50:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import HeroSection from './Hero';
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <HeroSection/>
+    
+    </div>
+  );
+}
+
+export default App;
